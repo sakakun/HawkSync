82283fd4b1572c187bea7fd0b245c3142f8cc5f3
Matthew Bate <matthew.bate@sk-projects.net>
2026-01-17 21:20:50 -0500

    Add NetLimiter to Git

---
+++ NetLimiterBridge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace NetLimiterBridge
+{
+    internal class Program
+    {
+
+	    public static async Task Main(string[] args)
+	    {
+		    // Create an instance of the NetLimiterBridge class
+		    var bridge = new NetLimiterBridge();
+
+		    // Connection parameters
+		    string hostname = "38.54.101.108";
+		    ushort port = 9298;
+		    string username = "rssaka";
+		    string password = "Dk75Rn43s!";
+
+		    // Run the bridge service
+		    await bridge.RunAsync(hostname, port, username, password);
+	    }
+
+    }
+}
