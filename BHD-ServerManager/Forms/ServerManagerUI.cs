0e83c33315c4640e8d7e0117dac608a22be85dab
Matthew Bate <matthew.bate@outlook.com>
2026-02-05 23:26:55 -0500

    Created and Independant Ticker for the Main Window.
    Commented out loading of tab settings.  This will be handled by the tab init.

--- BHD-ServerManager/Forms/ServerManagerUI.cs
+++ BHD-ServerManager/Forms/ServerManagerUI.cs
@@ -14,19 +14,19 @@ namespace BHD_ServerManager.Forms
 {
     public partial class ServerManagerUI : Form
     {
-        // The Instances (Data)
+
         private static theInstance thisInstance => CommonCore.theInstance!;
         private static playerInstance playerInstance => CommonCore.instancePlayers!;
 
         // Server Manager Tabs
-        public tabProfile ProfileTab = null!;                   // The Profile Tab User Control
-        public tabGamePlay  ServerTab  = null!;                 // The Server Tab User Control
-        public tabMaps    MapsTab    = null!;                   // The Maps Tab User Control
-        public tabPlayers PlayersTab = null!;                   // The Players Tab User Control
-        public tabChat    ChatTab    = null!;                   // The Chat Tab User Control
-        public tabBans    BanTab     = null!;                   // The Bans Tab User Control
-        public tabStats   StatsTab   = null!;                   // The Stats Tab User Control
-        public tabAdmin   AdminTab   = null!;                   // The Admin Tab User Control
+        public tabProfile       ProfileTab = null!;                   // The Profile Tab User Control
+        public tabGamePlay      ServerTab  = null!;                   // The Server Tab User Control
+        public tabMaps          MapsTab    = null!;                   // The Maps Tab User Control
+        public tabPlayers       PlayersTab = null!;                   // The Players Tab User Control
+        public tabChat          ChatTab    = null!;                   // The Chat Tab User Control
+        public tabBans          BanTab     = null!;                   // The Bans Tab User Control
+        public tabStats         StatsTab   = null!;                   // The Stats Tab User Control
+        public tabAdmin         AdminTab   = null!;                   // The Admin Tab User Control
 
         public ServerManagerUI()
         {
@@ -35,22 +35,6 @@ namespace BHD_ServerManager.Forms
         }
 
         private void PostServerManagerInitalization(object? sender, EventArgs e)
-        {
-            // Load the Panels
-            functionEvent_loadPanels();            
-
-			// Old Server Settings Initialization
-            chatInstanceManager.LoadSettings();
-			theInstanceManager.InitializeTickers();
-            theInstanceManager.LoadWebStatsSettings();
-            adminInstanceManager.InitializeDefaultAdmin();
-
-			// New Server Settings Initialization
-			ProfileTab.methodFunction_loadSettings();
-
-        }
-
-        private void functionEvent_loadPanels()
         {
             // Load the User Controls into the TabPages
             tabProfile.Controls.Add(ProfileTab = new tabProfile());
@@ -61,11 +45,48 @@ namespace BHD_ServerManager.Forms
             tabBans.Controls.Add(BanTab = new tabBans());
             tabStats.Controls.Add(StatsTab = new tabStats());
             tabAdmin.Controls.Add(AdminTab = new tabAdmin());
+
+            // Initialize "Global" Tickers for the Server Manager
+            theInstanceManager.InitializeTickers();
+
+            // Initialize Ticker for the Server Manager UI
+            CommonCore.Ticker?.Start("ServerManager", 1000, Ticker_ServerManagerUI);
+
+            // Old Server Settings Initialization - each item should be responsible for its own loading. 
+            // chatInstanceManager.LoadSettings();
+            // theInstanceManager.LoadWebStatsSettings();
+            // adminInstanceManager.InitializeDefaultAdmin();
+
         }
 
-        // --- Server Status and Controls ---
-        public void functionEvent_tickerServerGUI()
+        /// <summary>
+        /// Independent ticker method for updating the Server Manager UI elements.
+        /// </summary>
+        /// <remarks>If called from a thread other than the UI thread, the method automatically marshals
+        /// the update to the UI thread. This ensures thread-safe updates to UI elements.</remarks>
+        public void Ticker_ServerManagerUI()
         {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(Ticker_ServerManagerUI));
+                return;
+            }
+
+            // Update Label for Win Condition
+            UpdateStatusLabels();
+        }
+
+        /// <summary>
+        /// Updates the status labels in the user interface to reflect the current server state, player counts, scores,
+        /// win conditions, and remaining game time.
+        /// </summary>
+        /// <remarks>This method should be called whenever the game state changes to ensure that the
+        /// displayed information remains accurate. If the server is offline, only offline-related labels are updated.
+        /// For active games, the labels are updated based on the current game type and relevant statistics. This method
+        /// is not thread-safe and should be invoked from the UI thread.</remarks>
+        private void UpdateStatusLabels()
+        {
+
             // Update the Server Status Text
             toolStripStatus.Text = thisInstance.instanceStatus switch
             {
@@ -76,152 +97,118 @@ namespace BHD_ServerManager.Forms
                 InstanceStatus.STARTDELAY => "Server is running. Game ready, waiting for start.",
                 _ => toolStripStatus.Text
             };
-            // Update Label for Win Condition
-            functionEvent_UpdateStatusLabels();
-        }
 
-        private void functionEvent_UpdateStatusLabels()
-        {
-            // Offline Labels
             if (thisInstance.instanceStatus == InstanceStatus.OFFLINE)
             {
-                label_PlayersOnline.Text = "[Players Online]";
-                label_BlueScore.Text = "[Blue Score]";
-                label_RedScore.Text = "[Red Score]";
-                label_WinCondition.Text = "[Win Condition]";
-                label_TimeLeft.Text = "[Time Left]";
+                SetOfflineLabels();
                 return;
             }
-            // Variables
-            int scoreTotal = 0;
-            string blueScore = string.Empty;
-            string redScore = string.Empty;
-            string playerName = string.Empty;
-            PlayerObject? topPlayer = null;
-            string winConditions = string.Empty;
 
-            // Player Online Label
             label_PlayersOnline.Text = $"[{thisInstance.gameInfoNumPlayers}/{thisInstance.gameMaxSlots}]";
+
+            string blueScore = "[NO]";
+            string redScore = "[PLAYERS]";
+            string winConditions = string.Empty;
 
             if (playerInstance.PlayerList.Count > 0)
             {
-                winConditions = $"[{thisInstance.gameInfoWinCond} Kills ({thisInstance.gameInfoGameType})]";
+                int gameType = thisInstance.gameInfoGameType;
+                int winCond = thisInstance.gameInfoWinCond;
+                int blue = thisInstance.gameInfoBlueScore;
+                int red = thisInstance.gameInfoRedScore;
+                bool isBlueDefending = thisInstance.gameInfoIsBlueDefending;
 
-                if (thisInstance.gameInfoGameType == 0)
+                switch (gameType)
                 {
-                    topPlayer = playerInstance.PlayerList.Values.OrderByDescending(p => p.stat_Kills).FirstOrDefault();
-                    scoreTotal = topPlayer!.stat_Kills;
-                    playerName = (scoreTotal == 0 ? "Draw" : topPlayer!.PlayerName);
-                    blueScore = $"{scoreTotal}";
-                    redScore = $"{playerName}";
-                } else if (thisInstance.gameInfoGameType == 4)
-                {
-                    topPlayer = playerInstance.PlayerList.Values.OrderByDescending(p => p.ActiveZoneTime).FirstOrDefault();
-                    scoreTotal = topPlayer!.ActiveZoneTime;
-                    playerName = ( scoreTotal == 0 ? "Draw" : topPlayer!.PlayerName);
-                    blueScore = $"{TimeSpan.FromSeconds(scoreTotal):hh\\:mm\\:ss}";
-                    redScore = $"{playerName}";
-                    winConditions = $"[Time of {TimeSpan.FromSeconds(thisInstance.gameInfoWinCond*60):hh\\:mm\\:ss} ({thisInstance.gameInfoGameType})]";
-                } else if (thisInstance.gameInfoGameType == 5)
-                {
-                    blueScore = $"{thisInstance.gameInfoBlueScore}";
-                    redScore = $"{thisInstance.gameInfoRedScore}";
-                    winConditions = $"[{thisInstance.gameInfoWinCond} Targets ({thisInstance.gameInfoGameType})]";
-                } else if (thisInstance.gameInfoGameType == 6)
-                {
-                    blueScore = $"{(thisInstance.gameInfoIsBlueDefending == false ? "Red Attacking" : thisInstance.gameInfoBlueScore) }";
-                    redScore = $"{(thisInstance.gameInfoIsBlueDefending ? "Blue Attacking" : thisInstance.gameInfoRedScore )}";
-                    winConditions = $"[{thisInstance.gameInfoWinCond} Targets ({thisInstance.gameInfoGameType})]";
-                } else if (thisInstance.gameInfoGameType == 3)
-                {
-                    blueScore = $"{TimeSpan.FromSeconds(thisInstance.gameInfoBlueScore * 60):hh\\:mm\\:ss}";
-                    redScore = $"{TimeSpan.FromSeconds(thisInstance.gameInfoRedScore * 60):hh\\:mm\\:ss}";
-                    winConditions = $"[Time of {TimeSpan.FromSeconds(thisInstance.gameInfoWinCond * 60):hh\\:mm\\:ss} ({thisInstance.gameInfoGameType})]";
-                } else if (thisInstance.gameInfoGameType == 7 || thisInstance.gameInfoGameType == 8)
-                {
-                    blueScore = $"{thisInstance.gameInfoBlueScore}";
-                    redScore = $"{thisInstance.gameInfoRedScore}";
-                    winConditions = $"[{thisInstance.gameInfoWinCond} Captures ({thisInstance.gameInfoGameType})]";
-                } else if (thisInstance.gameInfoGameType == 1)
-                {
-                    blueScore = $"{thisInstance.gameInfoBlueScore}";
-                    redScore = $"{thisInstance.gameInfoRedScore}";
-                    winConditions = $"[{thisInstance.gameInfoWinCond} Kills ({thisInstance.gameInfoGameType})]";
-                } else
-                {
-                    blueScore = $"{thisInstance.gameInfoBlueScore}";
-                    redScore = $"{thisInstance.gameInfoRedScore}";
-                    winConditions = $"[{thisInstance.gameInfoWinCond} Kills ({thisInstance.gameInfoGameType})]";
+                    case 0:
+                        var topPlayerKills = playerInstance.PlayerList.Values.OrderByDescending(p => p.stat_Kills).FirstOrDefault();
+                        int scoreTotal = topPlayerKills?.stat_Kills ?? 0;
+                        string playerName = (scoreTotal == 0 ? "Draw" : topPlayerKills?.PlayerName ?? "Unknown");
+                        blueScore = $"{scoreTotal}";
+                        redScore = $"{playerName}";
+                        winConditions = $"[{winCond} Kills ({gameType})]";
+                        break;
+                    case 4:
+                        var topPlayerZone = playerInstance.PlayerList.Values.OrderByDescending(p => p.ActiveZoneTime).FirstOrDefault();
+                        int zoneTime = topPlayerZone?.ActiveZoneTime ?? 0;
+                        string zonePlayer = (zoneTime == 0 ? "Draw" : topPlayerZone?.PlayerName ?? "Unknown");
+                        blueScore = $"{TimeSpan.FromSeconds(zoneTime):hh\\:mm\\:ss}";
+                        redScore = $"{zonePlayer}";
+                        winConditions = $"[Time of {TimeSpan.FromSeconds(winCond * 60):hh\\:mm\\:ss} ({gameType})]";
+                        break;
+                    case 5:
+                        blueScore = $"{blue}";
+                        redScore = $"{red}";
+                        winConditions = $"[{winCond} Targets ({gameType})]";
+                        break;
+                    case 6:
+                        blueScore = $"{(isBlueDefending == false ? "Red Attacking" : blue)}";
+                        redScore = $"{(isBlueDefending ? "Blue Attacking" : red)}";
+                        winConditions = $"[{winCond} Targets ({gameType})]";
+                        break;
+                    case 3:
+                        blueScore = $"{TimeSpan.FromSeconds(blue * 60):hh\\:mm\\:ss}";
+                        redScore = $"{TimeSpan.FromSeconds(red * 60):hh\\:mm\\:ss}";
+                        winConditions = $"[Time of {TimeSpan.FromSeconds(winCond * 60):hh\\:mm\\:ss} ({gameType})]";
+                        break;
+                    case 7:
+                    case 8:
+                        blueScore = $"{blue}";
+                        redScore = $"{red}";
+                        winConditions = $"[{winCond} Captures ({gameType})]";
+                        break;
+                    case 1:
+                        blueScore = $"{blue}";
+                        redScore = $"{red}";
+                        winConditions = $"[{winCond} Kills ({gameType})]";
+                        break;
+                    default:
+                        blueScore = $"{blue}";
+                        redScore = $"{red}";
+                        winConditions = $"[{winCond} Kills ({gameType})]";
+                        break;
                 }
-
-
-                label_BlueScore.Text = $"[{blueScore}]";
-                label_RedScore.Text = $"[{redScore}]";
-
-            } else
-            {
-                label_BlueScore.Text = "[NO]";
-                label_RedScore.Text = "[PLAYERS]";
             }
 
+            label_BlueScore.Text = $"[{blueScore}]";
+            label_RedScore.Text = $"[{redScore}]";
             label_WinCondition.Text = winConditions;
-            label_TimeLeft.Text = "[ "+ thisInstance.gameInfoTimeRemaining.ToString(@"hh\:mm\:ss") + " ]";
+            label_TimeLeft.Text = "[ " + thisInstance.gameInfoTimeRemaining.ToString(@"hh\:mm\:ss") + " ]";
+        }
+
+        private void SetOfflineLabels()
+        {
+            label_PlayersOnline.Text = "[Players Online]";
+            label_BlueScore.Text = "[Blue Score]";
+            label_RedScore.Text = "[Red Score]";
+            label_WinCondition.Text = "[Win Condition]";
+            label_TimeLeft.Text = "[Time Left]";
         }
 
         // --- Form Closing ---
         protected override void OnFormClosing(FormClosingEventArgs e)
         {
+            // Confirm Exit
             var result = MessageBox.Show(
                 "Are you sure you want to exit?",
                 "Confirm Exit",
                 MessageBoxButtons.YesNo,
                 MessageBoxIcon.Question);
 
+            // Nevermind and return to the Server Manager
             if (result == DialogResult.No)
             {
                 e.Cancel = true;
                 return;
             }
-            NetLimiterClient.StopBridgeProcess();
 
-            CommonCore.Ticker?.Stop("ServerManager");
-            CommonCore.Ticker?.Stop("ChatManager");
-            CommonCore.Ticker?.Stop("PlayerManager");
-            CommonCore.Ticker?.Stop("BanManager");
+            // Stop all Tickers
+            CommonCore.Ticker?.StopAll();
+            // Stop the NetLimiter Bridge Process if it's running
+            NetLimiterClient.StopBridgeProcess();
+            // Close Application
             base.OnFormClosing(e);
         }
 
-        // --- About Window Start---
-        // Win32 API constants and methods
-        private const int WM_SYSCOMMAND = 0x112;
-        private const int ABOUT_SYSMENU_ID = 0x1FFF; // Custom ID for AboutWindow
-        [DllImport("user32.dll")]
-        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
-        [DllImport("user32.dll")]
-        private static extern bool InsertMenu(IntPtr hMenu, int wPosition, int wFlags, int wIDNewItem, string lpNewItem);
-        private const int MF_STRING = 0x00000000;
-        private const int MF_SEPARATOR = 0x00000800;
-        protected override void OnHandleCreated(EventArgs e)
-        {
-            base.OnHandleCreated(e);
-
-            // Get the system menu handle
-            IntPtr systemMenuHandle = GetSystemMenu(this.Handle, false);
-
-            // Add a separator and the AboutWindow item
-            InsertMenu(systemMenuHandle, -1, MF_SEPARATOR, 0, string.Empty);
-            InsertMenu(systemMenuHandle, -1, MF_STRING, ABOUT_SYSMENU_ID, "About");
-        }
-        protected override void WndProc(ref Message m)
-        {
-            if (m.Msg == WM_SYSCOMMAND && (int)m.WParam == ABOUT_SYSMENU_ID)
-            {
-                var about = new AboutWindow();
-                about.ShowDialog();
-                return;
-            }
-            base.WndProc(ref m);
-        }
-        // --- About Window End ---
     }
 }
