66ee09a79f8d936676bc70711ee3e94fa5c9d4af
Matthew Bate <matthew.bate@sk-projects.net>
2026-02-13 15:11:30 -0500

    Same changes to remote.

--- RemoteClient/Forms/Panels/tabBans.Designer.cs
+++ RemoteClient/Forms/Panels/tabBans.Designer.cs
@@ -120,10 +120,7 @@
             textBox_countryCode = new TextBox();
             textBox_countryName = new TextBox();
             btn_proxyAddCountry = new Button();
-            tabControl1 = new TabControl();
-            proxyLookup = new TabPage();
-            tabPage2 = new TabPage();
-            tableLayoutPanel14 = new TableLayoutPanel();
+            proxySettings = new TableLayoutPanel();
             groupBox12 = new GroupBox();
             tableLayoutPanel19 = new TableLayoutPanel();
             cb_serviceIP2LocationIO = new CheckBox();
@@ -230,9 +227,7 @@
             tableLayoutPanel17.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgProxyCountryBlockList).BeginInit();
             tableLayoutPanel15.SuspendLayout();
-            tabControl1.SuspendLayout();
-            tabPage2.SuspendLayout();
-            tableLayoutPanel14.SuspendLayout();
+            proxySettings.SuspendLayout();
             groupBox12.SuspendLayout();
             tableLayoutPanel19.SuspendLayout();
             panel3.SuspendLayout();
@@ -1321,7 +1316,7 @@
             tableLayoutPanel16.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 10F));
             tableLayoutPanel16.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
             tableLayoutPanel16.Controls.Add(tableLayoutPanel17, 0, 0);
-            tableLayoutPanel16.Controls.Add(tabControl1, 2, 0);
+            tableLayoutPanel16.Controls.Add(proxySettings, 2, 0);
             tableLayoutPanel16.Dock = DockStyle.Fill;
             tableLayoutPanel16.Location = new Point(3, 3);
             tableLayoutPanel16.Name = "tableLayoutPanel16";
@@ -1440,57 +1435,24 @@
             btn_proxyAddCountry.UseVisualStyleBackColor = true;
             btn_proxyAddCountry.Click += btn_proxyAddCountry_Click;
             // 
-            // tabControl1
+            // proxySettings
             // 
-            tabControl1.Controls.Add(proxyLookup);
-            tabControl1.Controls.Add(tabPage2);
-            tabControl1.Dock = DockStyle.Fill;
-            tabControl1.Location = new Point(263, 3);
-            tabControl1.Multiline = true;
-            tabControl1.Name = "tabControl1";
-            tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(686, 382);
-            tabControl1.TabIndex = 1;
-            // 
-            // proxyLookup
-            // 
-            proxyLookup.Location = new Point(4, 24);
-            proxyLookup.Name = "proxyLookup";
-            proxyLookup.Padding = new Padding(3);
-            proxyLookup.Size = new Size(678, 354);
-            proxyLookup.TabIndex = 0;
-            proxyLookup.Text = "IP Address Lookup";
-            proxyLookup.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            tabPage2.Controls.Add(tableLayoutPanel14);
-            tabPage2.Location = new Point(4, 24);
-            tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(678, 354);
-            tabPage2.TabIndex = 1;
-            tabPage2.Text = "Settings";
-            tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // tableLayoutPanel14
-            // 
-            tableLayoutPanel14.ColumnCount = 5;
-            tableLayoutPanel14.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 250F));
-            tableLayoutPanel14.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel14.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200F));
-            tableLayoutPanel14.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel14.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            tableLayoutPanel14.Controls.Add(groupBox12, 2, 0);
-            tableLayoutPanel14.Controls.Add(panel3, 0, 0);
-            tableLayoutPanel14.Dock = DockStyle.Fill;
-            tableLayoutPanel14.Location = new Point(3, 3);
-            tableLayoutPanel14.Margin = new Padding(0);
-            tableLayoutPanel14.Name = "tableLayoutPanel14";
-            tableLayoutPanel14.RowCount = 1;
-            tableLayoutPanel14.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel14.Size = new Size(672, 348);
-            tableLayoutPanel14.TabIndex = 0;
+            proxySettings.ColumnCount = 5;
+            proxySettings.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 250F));
+            proxySettings.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            proxySettings.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 200F));
+            proxySettings.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            proxySettings.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            proxySettings.Controls.Add(groupBox12, 2, 0);
+            proxySettings.Controls.Add(panel3, 0, 0);
+            proxySettings.Dock = DockStyle.Fill;
+            proxySettings.Location = new Point(260, 0);
+            proxySettings.Margin = new Padding(0);
+            proxySettings.Name = "proxySettings";
+            proxySettings.RowCount = 1;
+            proxySettings.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            proxySettings.Size = new Size(692, 388);
+            proxySettings.TabIndex = 0;
             // 
             // groupBox12
             // 
@@ -1499,7 +1461,7 @@
             groupBox12.Location = new Point(273, 3);
             groupBox12.Name = "groupBox12";
             groupBox12.Padding = new Padding(0);
-            groupBox12.Size = new Size(194, 342);
+            groupBox12.Size = new Size(194, 382);
             groupBox12.TabIndex = 1;
             groupBox12.TabStop = false;
             groupBox12.Text = "Service Providers";
@@ -1523,7 +1485,7 @@
             tableLayoutPanel19.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
             tableLayoutPanel19.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
             tableLayoutPanel19.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel19.Size = new Size(194, 326);
+            tableLayoutPanel19.Size = new Size(194, 366);
             tableLayoutPanel19.TabIndex = 0;
             // 
             // cb_serviceIP2LocationIO
@@ -1564,13 +1526,13 @@
             panel3.Location = new Point(0, 0);
             panel3.Margin = new Padding(0);
             panel3.Name = "panel3";
-            panel3.Size = new Size(250, 348);
+            panel3.Size = new Size(250, 388);
             panel3.TabIndex = 2;
             // 
             // groupBox15
             // 
             groupBox15.Controls.Add(tableLayoutPanel22);
-            groupBox15.Dock = DockStyle.Fill;
+            groupBox15.Dock = DockStyle.Top;
             groupBox15.Location = new Point(0, 232);
             groupBox15.Name = "groupBox15";
             groupBox15.Size = new Size(250, 54);
@@ -1643,7 +1605,7 @@
             // 
             groupBox14.Controls.Add(tableLayoutPanel21);
             groupBox14.Dock = DockStyle.Bottom;
-            groupBox14.Location = new Point(0, 286);
+            groupBox14.Location = new Point(0, 326);
             groupBox14.Name = "groupBox14";
             groupBox14.Size = new Size(250, 62);
             groupBox14.TabIndex = 2;
@@ -2506,9 +2468,7 @@
             ((System.ComponentModel.ISupportInitialize)dgProxyCountryBlockList).EndInit();
             tableLayoutPanel15.ResumeLayout(false);
             tableLayoutPanel15.PerformLayout();
-            tabControl1.ResumeLayout(false);
-            tabPage2.ResumeLayout(false);
-            tableLayoutPanel14.ResumeLayout(false);
+            proxySettings.ResumeLayout(false);
             groupBox12.ResumeLayout(false);
             tableLayoutPanel19.ResumeLayout(false);
             tableLayoutPanel19.PerformLayout();
@@ -2621,10 +2581,8 @@
         private TextBox textBox_countryCode;
         private TextBox textBox_countryName;
         private Button btn_proxyAddCountry;
-        private TabControl tabControl1;
-        private TabPage proxyLookup;
-        private TabPage tabPage2;
-        private TableLayoutPanel tableLayoutPanel14;
+        private TabControl proxyTabs;
+        private TableLayoutPanel proxySettings;
         private GroupBox groupBox11;
         private TableLayoutPanel tableLayoutPanel18;
         private CheckBox cb_enableProxyCheck;
