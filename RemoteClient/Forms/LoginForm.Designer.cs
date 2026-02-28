52d2f919e2725205458072d735c337a21f53fd97
Matthew Bate <matthew.bate@outlook.com>
2026-02-28 17:59:11 -0500

    Icon Update for HawkSync

--- RemoteClient/Forms/LoginForm.Designer.cs
+++ RemoteClient/Forms/LoginForm.Designer.cs
@@ -11,126 +11,130 @@ partial class LoginForm
     private Button btnCancel;
     private Label lblStatus;
 
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        
-        // Form
-        this.Text = "HawkSync Remote - Login";
-        this.Size = new Size(450, 320);
-        this.StartPosition = FormStartPosition.CenterScreen;
-        this.FormBorderStyle = FormBorderStyle.FixedDialog;
-        this.MaximizeBox = false;
-        this.MinimizeBox = false;
+	private void InitializeComponent()
+	{
+		System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+		lblTitle = new Label();
+		lblServerUrl = new Label();
+		txtServerUrl = new TextBox();
+		lblUsername = new Label();
+		txtUsername = new TextBox();
+		lblPassword = new Label();
+		txtPassword = new TextBox();
+		chkRememberServer = new CheckBox();
+		lblStatus = new Label();
+		btnLogin = new Button();
+		btnCancel = new Button();
+		SuspendLayout();
+		// 
+		// lblTitle
+		// 
+		lblTitle.Location = new Point(0, 0);
+		lblTitle.Name = "lblTitle";
+		lblTitle.Size = new Size(100, 23);
+		lblTitle.TabIndex = 0;
+		// 
+		// lblServerUrl
+		// 
+		lblServerUrl.Location = new Point(0, 0);
+		lblServerUrl.Name = "lblServerUrl";
+		lblServerUrl.Size = new Size(100, 23);
+		lblServerUrl.TabIndex = 1;
+		// 
+		// txtServerUrl
+		// 
+		txtServerUrl.Location = new Point(0, 0);
+		txtServerUrl.Name = "txtServerUrl";
+		txtServerUrl.Size = new Size(100, 23);
+		txtServerUrl.TabIndex = 2;
+		// 
+		// lblUsername
+		// 
+		lblUsername.Location = new Point(0, 0);
+		lblUsername.Name = "lblUsername";
+		lblUsername.Size = new Size(100, 23);
+		lblUsername.TabIndex = 3;
+		// 
+		// txtUsername
+		// 
+		txtUsername.Location = new Point(0, 0);
+		txtUsername.Name = "txtUsername";
+		txtUsername.Size = new Size(100, 23);
+		txtUsername.TabIndex = 4;
+		// 
+		// lblPassword
+		// 
+		lblPassword.Location = new Point(0, 0);
+		lblPassword.Name = "lblPassword";
+		lblPassword.Size = new Size(100, 23);
+		lblPassword.TabIndex = 5;
+		// 
+		// txtPassword
+		// 
+		txtPassword.Location = new Point(0, 0);
+		txtPassword.Name = "txtPassword";
+		txtPassword.Size = new Size(100, 23);
+		txtPassword.TabIndex = 6;
+		// 
+		// chkRememberServer
+		// 
+		chkRememberServer.Location = new Point(0, 0);
+		chkRememberServer.Name = "chkRememberServer";
+		chkRememberServer.Size = new Size(104, 24);
+		chkRememberServer.TabIndex = 7;
+		// 
+		// lblStatus
+		// 
+		lblStatus.Location = new Point(0, 0);
+		lblStatus.Name = "lblStatus";
+		lblStatus.Size = new Size(100, 23);
+		lblStatus.TabIndex = 8;
+		// 
+		// btnLogin
+		// 
+		btnLogin.Location = new Point(0, 0);
+		btnLogin.Name = "btnLogin";
+		btnLogin.Size = new Size(75, 23);
+		btnLogin.TabIndex = 9;
+		btnLogin.Click += BtnLogin_Click;
+		// 
+		// btnCancel
+		// 
+		btnCancel.Location = new Point(0, 0);
+		btnCancel.Name = "btnCancel";
+		btnCancel.Size = new Size(75, 23);
+		btnCancel.TabIndex = 10;
+		btnCancel.Click += BtnCancel_Click;
+		// 
+		// LoginForm
+		// 
+		AcceptButton = btnLogin;
+		CancelButton = btnCancel;
+		ClientSize = new Size(434, 281);
+		Controls.Add(lblTitle);
+		Controls.Add(lblServerUrl);
+		Controls.Add(txtServerUrl);
+		Controls.Add(lblUsername);
+		Controls.Add(txtUsername);
+		Controls.Add(lblPassword);
+		Controls.Add(txtPassword);
+		Controls.Add(chkRememberServer);
+		Controls.Add(lblStatus);
+		Controls.Add(btnLogin);
+		Controls.Add(btnCancel);
+		FormBorderStyle = FormBorderStyle.FixedDialog;
+		Icon = (Icon)resources.GetObject("$this.Icon");
+		MaximizeBox = false;
+		MinimizeBox = false;
+		Name = "LoginForm";
+		StartPosition = FormStartPosition.CenterScreen;
+		Text = "HawkSync Remote - Login";
+		ResumeLayout(false);
+		PerformLayout();
+	}
 
-        // Title Label
-        var lblTitle = new Label
-        {
-            Text = "HawkSync Remote Client",
-            Font = new Font("Segoe UI", 16F, FontStyle.Bold),
-            Location = new Point(20, 20),
-            Size = new Size(400, 30),
-            TextAlign = ContentAlignment.MiddleCenter
-        };
-
-        // Server URL
-        var lblServerUrl = new Label
-        {
-            Text = "Server URL:",
-            Location = new Point(30, 70),
-            Size = new Size(100, 23),
-            TextAlign = ContentAlignment.MiddleLeft
-        };
-
-        this.txtServerUrl = new TextBox
-        {
-            Location = new Point(140, 70),
-            Size = new Size(270, 23),
-            Text = "http://localhost:5000"
-        };
-
-        // Username
-        var lblUsername = new Label
-        {
-            Text = "Username:",
-            Location = new Point(30, 110),
-            Size = new Size(100, 23),
-            TextAlign = ContentAlignment.MiddleLeft
-        };
-
-        this.txtUsername = new TextBox
-        {
-            Location = new Point(140, 110),
-            Size = new Size(270, 23)
-        };
-
-        // Password
-        var lblPassword = new Label
-        {
-            Text = "Password:",
-            Location = new Point(30, 150),
-            Size = new Size(100, 23),
-            TextAlign = ContentAlignment.MiddleLeft
-        };
-
-        this.txtPassword = new TextBox
-        {
-            Location = new Point(140, 150),
-            Size = new Size(270, 23),
-            UseSystemPasswordChar = true
-        };
-
-        // Remember Server
-        this.chkRememberServer = new CheckBox
-        {
-            Text = "Remember server and username",
-            Location = new Point(140, 185),
-            Size = new Size(250, 23)
-        };
-
-        // Status Label
-        this.lblStatus = new Label
-        {
-            Text = "Ready to connect",
-            Location = new Point(30, 215),
-            Size = new Size(380, 20),
-            TextAlign = ContentAlignment.MiddleLeft,
-            ForeColor = Color.Gray
-        };
-
-        // Login Button
-        this.btnLogin = new Button
-        {
-            Text = "Login",
-            Location = new Point(220, 245),
-            Size = new Size(90, 30),
-            DialogResult = DialogResult.None
-        };
-        this.btnLogin.Click += BtnLogin_Click;
-
-        // Cancel Button
-        this.btnCancel = new Button
-        {
-            Text = "Cancel",
-            Location = new Point(320, 245),
-            Size = new Size(90, 30),
-            DialogResult = DialogResult.Cancel
-        };
-        this.btnCancel.Click += BtnCancel_Click;
-
-        // Add controls
-        this.Controls.AddRange(new Control[]
-        {
-            lblTitle,
-            lblServerUrl, txtServerUrl,
-            lblUsername, txtUsername,
-            lblPassword, txtPassword,
-            chkRememberServer,
-            lblStatus,
-            btnLogin, btnCancel
-        });
-
-        this.AcceptButton = btnLogin;
-        this.CancelButton = btnCancel;
-    }
+	private Label lblTitle;
+	private Label lblServerUrl;
+	private Label lblUsername;
+	private Label lblPassword;
 }
